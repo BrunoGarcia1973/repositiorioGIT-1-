1e329fa4b59e510fcd69c1480e242bb45d0450f7
belucarretero <164357431+belucarretero@users.noreply.github.com>
2024-05-29 23:15:04 -0300

    Update capitulosmock.js

--- Grupo-3K03-08.zip/routes/capitulosmock.js
+++ Grupo-3K03-08.zip/routes/capitulosmock.js
@@ -3,54 +3,54 @@ const router = express.Router();
 
 let arr_SeriesMock = [
   {
-    CodigoProd: 0123,
-    Nombre: 'Rodrigo',
-    Fecha_nacimiento: '1973-05-24',
+    CodigoSerie: 2354,
+    Nombre: 'De Repente echo de Menos a Todos',
+    Duracion: '42:00',
   },
   {
-    CodigoProd: 0223,
-    Nombre: 'Ulises',
-    Fecha_nacimiento: '1986-05-22',
+    CodigoSerie: 9188,
+    Nombre: 'Written In The Stars',
+    Duracion: '43:00',
   },
   {
-    CodigoProd: 0323,
-    Nombre: 'Cristian',
-    Fecha_nacimiento: '1970-05-24',
+    CodigoSerie: 0323,
+    Nombre: 'The Lost Boy',
+    Duracion: '42:00',
   },
   {
-    CodigoProd: 0423,
-    Nombre: 'Damian',
-    Fecha_nacimiento: '1990-04-17',
+    CodigoSerie: 0423,
+    Nombre: 'El Asistente de Rachel',
+    Duracion: '22:00',
   },
   {
-    CodigoProd: 0523,
-    Nombre: 'Euguenia',
-    Fecha_nacimiento: '1950-12-24',
+    CodigoSerie: 0523,
+    Nombre: 'Terremoto',
+    Duracion: '22:00',
   },
   {
-    CodigoProd: 0623,
-    Nombre: 'Magali',
-    Fecha_nacimiento: '1973-05-24',
+    CodigoSerie: 0623,
+    Nombre: 'The Gift',
+    Duracion: '53:00',
   },
   {
-    CodigoProd: 0723,
-    Nombre: 'Cristina',
-    Fecha_nacimiento: '2000-05-30',
+    CodigoSerie: 0723,
+    Nombre: 'Amor perdido',
+    Duracion: '57:00',
   },
   {
-    CodigoProd: 0823,
-    Nombre: 'Rufino',
-    Fecha_nacimiento: '1952-02-01',
+    CodigoSerie: 0823,
+    Nombre: 'Revolutions',
+    Duracion: '43:00',
   },
   {
-    CodigoProd: 0923,
-    Nombre: 'Anabela',
-    Fecha_nacimiento: '1973-05-22',
+    CodigoSerie: 0923,
+    Nombre: 'Lion in the Meadow',
+    Duracion: '59:00',
   },
   {
-    CodigoProd: 0233,
-    Nombre: 'Vanesa',
-    Fecha_nacimiento: '1993-11-19',
+    CodigoSerie: 0233,
+    Nombre: 'El Padrino',
+    Duracion: '23:00',
   },
 ];
 
@@ -58,50 +58,50 @@ router.get('/api/seriesmock', async function (req, res) {
   res.json(arr_SeriesMock);
 });
 
-router.get('/api/seriesmock/:codigoProd', async function (req, res) {
+router.get('/api/seriesmock/:codigoSerie', async function (req, res) {
   let prod = arr_SeriesMock.find(
-    (x) => x.CodigoProd == req.params.codigoProd
+    (x) => x.CodigoSerie == req.params.codigoSerie
   );
   if (prod) res.json(prod);
   else res.status(404).json({ message: 'Capitulo no encontrado' });
 });
 
 router.post('/api/seriesmock/', (req, res) => {
-  const { Nombre, Fecha_nacimiento } = req.body;
-  let parcial = {
+  const { Nombre, Duracion } = req.body;
+  let series = {
     Nombre,
-    Fecha_nacimiento,
-    CodigoProd: Math.floor(10000 + Math.random() * 90000),
+    Duracion,
+    CodigoSerie: Math.floor(10000 + Math.random() * 90000),
   };
 
   // aqui agregar a la coleccion
-  arr_SeriesMock.push(parcial);
+  arr_SeriesMock.push(series);
 
-  res.status(201).json(parcial);
+  res.status(201).json(series);
 });
 
-router.put('/api/seriesmock/:codigoProd', (req, res) => {
-  let prod = arr_SeriesMock.find(
-    (x) => x.CodigoProd == req.params.codigoProd
+router.put('/api/seriesmock/:codigoSerie', (req, res) => {
+  let cap = arr_SeriesMock.find(
+    (x) => x.CodigoSerie== req.params.codigoSerie
   );
 
-  if (prod) {
+  if (cap) {
     const { Nombre } = req.body;
-    prod.Nombre = Nombre;
+    cap.Nombre = Nombre;
     res.json({ message: 'Capitulo actualizado' });
   } else {
     res.status(404).json({ message: 'Capitulo no encontrado' });
   }
 });
 
-router.delete('/api/seriesmock/:codigoProd', (req, res) => {
-  let prod = arr_SeriesMock.find(
-    (x) => x.CodigoProd == req.params.codigoProd
+router.delete('/api/seriesmock/:codigoSerie', (req, res) => {
+  let cap = arr_SeriesMock.find(
+    (x) => x.CodigoSerie == req.params.codigoSerie
   );
 
-  if (prod) {
+  if (cap) {
     arr_SeriesMock = arr_SeriesMock.filter(
-      (x) => x.CodigoProd == req.params.codigoProd
+      (x) => x.CodigoSerie == req.params.codigoSerie
     );
     res.json({ message: 'Capitulo eliminado' });
   } else {
