cd8c67eaf1483ad20fc5b123a574c3010d8ec1ff
belucarretero <164357431+belucarretero@users.noreply.github.com>
2024-05-29 23:18:26 -0300

    Update seriesmock.js

--- Grupo-3K03-08.zip/routes/seriesmock.js
+++ Grupo-3K03-08.zip/routes/seriesmock.js
@@ -4,63 +4,63 @@ const router = express.Router();
 let arr_SeriesMock = [
   {
     Codigo: 11111,
-    CodigoProd: 2354,
+    CodigoSerie: 2354,
     Nombre: 'One Tree Hill',
-    Fecha_estreno: '2003-09-23',
+    FechaEstreno: '2003-09-23',
   },
   {
     Codigo: 22222,
-    CodigoProd: 9188,
+    CodigoSerie: 9188,
     Nombre: 'Gilmore Girls',
-    Fecha_lanzamiento: '2000-10-05',
+    FechaEstreno: '2000-10-05',
   },
   {
     Codigo: 33333,
-    CodigoProd: 0323,
+    CodigoSerie: 0323,
     Nombre: 'Gossip Girl',
-    Fecha_lanzamiento: '2007-09-19',
+    FechaEstreno: '2007-09-19',
   },
   {
     Codigo: 4444,
-    CodigoProd: 0423,
+    CodigoSerie: 0423,
     Nombre: 'Friends',
-    Fecha_ingreso: '1994-09-22',
+    FechaEstreno: '1994-09-22',
   },
   {
     Codigo: 55555,
-    CodigoProd: 0523,
+    CodigoSerie: 0523,
     Nombre: 'Modern Family',
-    Fecha_lanzamiento: '2009-09-23',
+    FechaEstreno: '2009-09-23',
   },
   {
     Codigo: 66666,
-    CodigoProd: 0623,
+    CodigoSerie: 0623,
     Nombre: 'Game Of Thrones',
-    Fecha_lanzamiento: '2011-04-17',
+    FechaEstreno: '2011-04-17',
   },
   {
     Codigo: 77777,
-    CodigoProd: 0723,
+    CodigoSerie: 0723,
     Nombre: 'The Summer I Turned Pretty',
-    Fecha_lanzamiento: '2022-06-17',
+    FechaEstreno: '2022-06-17',
   },
   {
     Codigo: 88888,
-    CodigoProd: 0823,
+    CodigoSerie: 0823,
     Nombre: 'My Life With The Walter Boys',
-    Fecha_lanzamiento: '2023-12-07',
+    FechaEstreno: '2023-12-07',
   },
   {
     Codigo: 99999,
-    CodigoProd: 0923,
+    CodigoSerie: 0923,
     Nombre: 'Succession',
-    Fecha_lanzamiento: '2018-06-03',
+    FechaEstreno: '2018-06-03',
   },
   {
     Codigo: 11112,
-    CodigoProd: 0233,
+    CodigoSerie: 0233,
     Nombre: 'How I Met Your Mother',
-    Fecha_lanzamiento: '2005-09-19',
+    FechaEstreno: '2005-09-19',
   },
 ];
 
@@ -68,9 +68,9 @@ router.get('/api/seriesmock', async function (req, res) {
   res.json(arr_SeriesMock);
 });
 
-router.get('/api/seriesmock/:codigo/:codProd', async function (req, res) {
+router.get('/api/seriesmock/:codigo/:codigoSerie', async function (req, res) {
   let ser = arr_SeriesMock.find(
-    (x) => x.Codigo == req.params.codigo && x.CodigoProd === req.params.codProd
+    (x) => x.Codigo == req.params.codigo && x.CodigoSerie === req.params.codigoSerie
   );
   if (ser) res.json(ser);
   else res.status(404).json({ message: 'Serie no encontrada' });
@@ -90,9 +90,9 @@ router.post('/api/seriesmock/', (req, res) => {
   res.status(201).json(ser);
 });
 
-router.put('/api/seriesmock/:codigo/:codProd', (req, res) => {
+router.put('/api/seriesmock/:codigo/:codigoSerie', (req, res) => {
   let ser = arr_SeriesMock.find(
-    (x) => x.Codigo == req.params.codigo && x.CodigoProd === req.params.codProd
+    (x) => x.Codigo == req.params.codigo && x.CodigoSerie === req.params.codigoSerie
   );
 
   if (ser) {
@@ -104,9 +104,9 @@ router.put('/api/seriesmock/:codigo/:codProd', (req, res) => {
   }
 });
 
-router.delete('/api/seriesmock/:codigo/:codProd', (req, res) => {
+router.delete('/api/seriesmock/:codigo/:codigoSerie', (req, res) => {
   let ser = arr_SeriesMock.find(
-    (x) => x.Codigo == req.params.codigo && x.CodigoProd === req.params.codProd
+    (x) => x.Codigo == req.params.codigo && x.CodigoSerie === req.params.codigoSerie
   );
 
   if (ser) {
