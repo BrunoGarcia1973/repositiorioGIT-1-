74e3f52a7fb3f79da5c2bce3e31192a140a00a7e
BrunoGarcia1973 <96596@sistemas.frc.utn.edu.ar>
2024-06-11 16:15:15 -0300

    carpetas front y back creadas

--- Grupo-3K03-08.zip/BACKEND/routes/documentalesmock.js
+++ Grupo-3K03-08.zip/BACKEND/routes/documentalesmock.js
@@ -4,61 +4,51 @@ const router = express.Router();
 let arr_DocumentalesMock = [
   {
     Codigo: 11111,
-    CodigoProd: 1123,
     Nombre: 'Animales salvajes',
     Fecha_lanzamiento: '2021-02-18',
   },
   {
     Codigo: 22222,
-    CodigoProd: 1223,
     Nombre: 'Animales pasivos',
     Fecha_lanzamiento: '2021-06-23',
   },
   {
     Codigo: 33333,
-    CodigoProd: 1323,
     Nombre: 'La primera guerra mundial',
     Fecha_lanzamiento: '2021-02-22',
   },
   {
     Codigo: 44444,
-    CodigoProd: 1423,
     Nombre: 'La vida de Hitler',
     Fecha_ingreso: '2019-05-24',
   },
   {
     Codigo: 55555,
-    CodigoProd: 1523,
     Nombre: 'El 2001 de Argentina',
     Fecha_lanzamiento: '2022-02-22',
   },
   {
     Codigo: 66666,
-    CodigoProd: 1623,
     Nombre: 'Historia Argentina',
     Fecha_lanzamiento: '2020-12-30',
   },
   {
     Codigo: 77777,
-    CodigoProd: 1723,
     Nombre: 'Etapa de industrializacion',
     Fecha_lanzamiento: '2023-09-01',
   },
   {
     Codigo: 88888,
-    CodigoProd: 1823,
     Nombre: 'El frio de la Antartida',
     Fecha_lanzamiento: '1997-08-25',
   },
   {
     Codigo: 99999,
-    CodigoProd: 1923,
     Nombre: 'La vida de Rodrigo Bueno El Potro',
     Fecha_lanzamiento: '2011-02-18',
   },
   {
     Codigo: 12345,
-    CodigoProd: 1233,
     Nombre: 'La historia del Cuarteto',
     Fecha_lanzamiento: '2020-06-19',
   },
