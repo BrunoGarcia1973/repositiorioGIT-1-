1d71432011312b58bd47f0ef6f362001c28c32cc
belu.carretero <94188@sistemas.frc.utn.edu.ar>
2024-06-11 16:16:09 -0300

    Cambios Belén

--- Grupo-3K03-08.zip/test/seguridad.test.js
+++ Grupo-3K03-08.zip/test/seguridad.test.js
@@ -82,3 +82,63 @@ describe("GET /api/productoraJWT", () => {
   });
 
 });
+
+describe("GET /api/seriesJWT", () => {
+
+  it("Devolveria error, porque falta token de autorización", async function () {
+    const res = await request(app).get("/api/seriesJWT");
+    expect(res.statusCode).toEqual(401);
+    expect(res.body.message).toEqual("Acceso denegado");
+  });
+
+  it("Devolveria error, porque el token no es válido", async function () {
+    const res = await request(app).get("/api/seriesJWT")
+    .set("Authorization", 'Bearer invalido');
+    expect(res.statusCode).toEqual(403);
+    expect(res.body.message).toEqual("token no es valido");
+  });
+
+  it("Devolvería todos las series, solo autorizado para administradores", async function () {
+    const res1 = await request(app)
+    .post("/api/login")
+    .set("Content-type", "application/json")
+    .send(usuarioAdmin);
+    expect(res1.statusCode).toEqual(200);
+    let token = res1.body.accessToken;
+
+    const res = await request(app)
+      .get("/api/seriesJWT")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(
+      // VER ESTA PARTE
+      expect.arrayContaining([
+        expect.objectContaining({
+          CodigoSerie: expect.any(Number),
+          CodigoCapitulo: expect.any(Number),
+          Nombre: expect.any(String),
+          FechaEstreno: expect.any(String),
+          Activo: expect.any(Boolean),
+        }),
+      ])
+    );
+  });
+
+  it("Devolvería error de autorizacion, porque solo están autorizados los administradores", async function () {
+    const res1 = await request(app)
+    .post("/api/login")
+    .set("Content-type", "application/json")
+    .send(usuarioMiembro);
+    expect(res1.statusCode).toEqual(200);
+    let token = res1.body.accessToken;
+
+    const res = await request(app)
+      .get("/api/seriesJWT")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(res.statusCode).toEqual(403);
+    expect(res.body.message).toEqual('usuario no autorizado!');
+  });
+
+});
